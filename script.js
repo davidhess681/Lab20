34ee967842d6779cdca1e6932f35b7cf98709b52
david <david.hess681@gmail.com>
2018-11-06 08:34:12 -0500

    first commit for Lab 20

---
+++ script.js
@@ -0,0 +1,23 @@
+/*var favNum;
+favNum = prompt("Please tell me your favorite number");
+alert("Your favorite number is " + favNum);*/
+
+/*
+var prompt1;
+while (true){
+prompt1 = prompt("Please enter your name:");
+if (prompt1.length < 2){
+  alert("Too short!")
+}
+else{
+  alert("Hello " + prompt1);
+  break;
+}
+}
+
+var el = document.getElementById("myparagraph");
+el.innerHTML = "some other text"*/
+
+function myFunction(){
+  document.getElementById("myButton").innerHTML = "YOU CLICKED ME!!!!1!1!";
+}
